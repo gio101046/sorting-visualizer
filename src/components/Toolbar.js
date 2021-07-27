4ff724ab5b76860dd420c2c35eaec10241fd4c28
Giovani Rodriguez <giovaniluisrodriguez@gmail.com>
2021-07-26 20:17:36 -0400

    feat: add bootstrap 5 to project

---
+++ src/components/Toolbar.js
@@ -0,0 +1,11 @@
+import 'styles/Toolbar.css'
+import React from 'react';
+import Button from 'react-bootstrap/Button'
+
+class Toolbar extends React.Component {
+    render() {
+        return <Button>Hello World!</Button>
+    }
+}
+
+export default Toolbar;
