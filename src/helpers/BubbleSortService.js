85258ddcc429537feb752f4371d0cc768662f22a
Giovani Rodriguez <giovaniluisrodriguez@gmail.com>
2021-07-27 19:04:53 -0400

    feat: add final bubble sort service

--- src/helpers/BubbleSortService.js
+++ src/helpers/BubbleSortService.js
@@ -1,26 +1,76 @@
-
 class BubbleSortService {
-    constructor(array) {
-        this.array = array;
-        this.currentIndex = 0;
-        this.swapped = false;
+    #array;
+    #currentIndex = 0;
+    #swapped = false;
 
-        // weird javascript this thing
-        this.sort = this.sort.bind(this);
+    constructor(array) {
+        this.#array = array.slice();
     }
 
+    isSorted() {
+        return !this.#swapped;
+    }
+
+    resetSwapFlag() {
+        this.#swapped = false;
+    }
+
+    next() {
+        if (this.#currentIndex < this.#array.length - 1) {
+            this.#currentIndex++;
+            return true;
+        }
+
+        this.#currentIndex = 0;
+        return false;
+    }
+
+    shouldSwap() {
+        return this.#array[this.#currentIndex] > this.#array[this.#currentIndex + 1];
+    }
+
+    swap() {
+        var temp = this.#array[this.#currentIndex];
+        this.#array[this.#currentIndex] = this.#array[this.#currentIndex + 1];
+        this.#array[this.#currentIndex + 1] = temp;
+        this.#swapped = true;
+    }
+
+    getArray() {
+        return this.#array.slice();
+    }
+
+    // TODO: remove this later, only here for reference
     sort() {
         let swapped;
         do {
             swapped = false;
-            for (var i = 0; i < this.array.length - 1; i++) {
-                if (this.array[i] > this.array[i + 1]) {
-                    var temp = this.array[i];
-                    this.array[i] = this.array[i + 1];
-                    this.array[i + 1] = temp;
+            for (var i = 0; i < this.#array.length - 1; i++) {
+                if (this.#array[i] > this.#array[i + 1]) {
+                    var temp = this.#array[i];
+                    this.#array[i] = this.#array[i + 1];
+                    this.#array[i + 1] = temp;
                     swapped = true;
                 }
             }
         } while (swapped);
     }
 }
+
+array = [10, 5, 8, 2, 4, 1, 3, 7, 6, 9];
+sortService = new BubbleSortService(array);
+
+console.log(sortService.getArray());
+sortService.sort();
+/*
+do {
+    sortService.resetSwapFlag();
+    do {
+        if (sortService.shouldSwap()) {
+            sortService.swap();
+        }
+    } while (sortService.next());
+} while(!sortService.isSorted());
+*/
+
+console.log(sortService.getArray());
